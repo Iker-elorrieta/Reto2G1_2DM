e87b6c1033ea3791f68a5cddbf5f3118ea144aaa
Izarovega <izaro.santiagove@elorrieta-errekamari.com>
2026-01-19 21:57:16 +0100

    cambios

--- springBt/src/main/java/com/example/springBt/PerfilService.java
+++ springBt/src/main/java/com/example/springBt/PerfilService.java
@@ -1,26 +1,63 @@
 package com.example.springBt;
 
+
+
+import org.hibernate.Hibernate;
+
 import org.hibernate.Session;
+
 import org.springframework.web.bind.annotation.PathVariable;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 import modelo.Users;
 
+
+
 @RestController 
+
 @RequestMapping("/api")
+
 public class PerfilService {
 
-public Users getPerfil(@PathVariable int id) {
-	
-	
-	try(Session session =HibernateUtil.getSessionFactory().openSession()){
-		return session.get(Users.class,id);
-	}
+
+
+@RequestMapping("/perfil/{id}")
+
+public Users getPerfil(@PathVariable("id") int id) {
 
 	
+
 	
+
+	try(Session session =HibernateUtil.getSessionFactory().openSession()){
+
+		Users user = session.get(Users.class, id);
+
+        if (user != null) {
+
+            Hibernate.initialize(user.getTipos());
+
+        }
+
+        return user;
+
+	}
+
+
+
 	
+
+	
+
+	
+
 }
+
 	
+
 }
